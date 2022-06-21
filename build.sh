8c90998c27e31f798faa4786c2f6f7f2a8d52907
Dennis Dollée <dennisdollee@gmail.com>
2022-06-21 19:04:28 +0200

    Copy files (instead of symlink) for deployment

--- build.sh
+++ build.sh
@@ -33,10 +33,17 @@ fi
 working_dir=$PWD
 
 # link to files needed for static page
-ln -s $working_dir/server/views/index.html $working_dir/deploy/index.html
-ln -s $working_dir/client/data $working_dir/deploy/data
-ln -s $working_dir/client/assets $working_dir/deploy/assets
-ln -s $working_dir/client/js/thirdparty $working_dir/deploy/js/thirdparty
-ln -s $working_dir/client/app/third-party $working_dir/deploy/app/third-party
-ln -s $working_dir/client/dist/build.js $working_dir/deploy/dist/build.js
-ln -s $working_dir/client/dist/build.js.map $working_dir/deploy/dist/build.js.map
+cp $working_dir/server/views/index.html $working_dir/deploy/index.html
+cp $working_dir/client/data $working_dir/deploy/data
+cp $working_dir/client/assets $working_dir/deploy/assets
+cp $working_dir/client/js/thirdparty $working_dir/deploy/js/thirdparty
+cp $working_dir/client/app/third-party $working_dir/deploy/app/third-party
+cp $working_dir/client/dist/build.js $working_dir/deploy/dist/build.js
+cp $working_dir/client/dist/build.js.map $working_dir/deploy/dist/build.js.map
+# ln -s $working_dir/server/views/index.html $working_dir/deploy/index.html
+# ln -s $working_dir/client/data $working_dir/deploy/data
+# ln -s $working_dir/client/assets $working_dir/deploy/assets
+# ln -s $working_dir/client/js/thirdparty $working_dir/deploy/js/thirdparty
+# ln -s $working_dir/client/app/third-party $working_dir/deploy/app/third-party
+# ln -s $working_dir/client/dist/build.js $working_dir/deploy/dist/build.js
+# ln -s $working_dir/client/dist/build.js.map $working_dir/deploy/dist/build.js.map
